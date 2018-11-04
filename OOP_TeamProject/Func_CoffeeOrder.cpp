3f7628436ac49a12c2eae96d37b7fd4062a1b478
Yeoilgoo <ilgooinkorea@gmail.com>
2018-11-04 21:59:05 +0900

    alpha ver. finished

--- OOP_TeamProject/Func_CoffeeOrder.cpp
+++ OOP_TeamProject/Func_CoffeeOrder.cpp
@@ -1,12 +1,23 @@
 #include "Func_CoffeeOrder.h"
 
+//global variables (in GlobalVar.cpp)
+extern std::vector<Coffee> defaultCoffeeList;
+
+
+/*
+주문을 받는 함수
+커피, 해당 커피에 추가하고 싶은 첨가물이나 원두를 선택하여 추가 가능
+
+*/
+
 void coffeeOrderSelected() {
-	std::vector<Coffee> orderedCoffeeList;
+	std::vector<Coffee> orderedCoffeeList; //주문한 커피 목록
 
 	unsigned int orderdCoffeeNum, optionalCoffeeBeanNum, optionalIngreNum;
 	char a = NULL, b = NULL, c = NULL;
 
 	for (int i = 0; ; i++) {
+		bool checkCustom = false; // 주문된 커피에 추가적인 수정이 가해졌는지를 체크.
 
 		std::cout << std::endl << "주문할 커피의 번호를 입력해 주세요." << std::endl;
 		printDefaultCoffeeList();
@@ -21,6 +32,7 @@ void coffeeOrderSelected() {
 				break;
 			}
 
+			checkCustom = true;
 			printDefaultCoffeeBeanList();
 			std::cout << "입력: ";
 			std::cin >> optionalCoffeeBeanNum;
@@ -34,21 +46,35 @@ void coffeeOrderSelected() {
 				break;
 			}
 
+			checkCustom = true;
 			printDefaultIngredientList();
 			std::cout << "입력: ";
 			std::cin >> optionalIngreNum;
 			orderedCoffeeList.at(i).addIngreList(getDefaultIngredient(optionalIngreNum - 1));
 		}
 
-		std::cout << std::endl << "메뉴를 추가하시겠습니까?(Y/N): ";
-		std::cin >> c;
-		if (c == 'N' || c == 'n') {
-			break;
+		if (checkCustom) {
+			std::cout << std::endl << "기본 메뉴에서 변동이 있는 상품이 존재합니다." << std::endl << "이 상품을 저장하시겠습니까?(Y/N): ";
+			std::cin >> c;
+			if (c == 'N' || c == 'n') {
+				break;
+			}
+
+			std::string _newCoffeeName;
+			std::cout << "추가할 커피의 이름을 입력해주세요: ";
+			std::cin >> _newCoffeeName;
+			Coffee _newCoffee = Coffee(_newCoffeeName, orderedCoffeeList.at(i).getBeanList(), orderedCoffeeList.at(i).getIngreList());
+			defaultCoffeeList.push_back(_newCoffee);
 		}
 	}
 
 	makeBill(orderedCoffeeList);
 }
+
+/*
+영수증 생성 함수
+주문한 커피와 그 가격을 출력
+*/
 
 void makeBill(const std::vector<Coffee> _orderedCoffeeList) {
 	std::cout << std::endl << "<주문서>" << std::endl;
